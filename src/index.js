fee74a9d81eca99003e596201d18b897cc8a24bf
thien-lebk <bhnhock@gmail.com>
2025-09-06 21:51:57 +0700

    Add swagger

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const server = require('./server');
 
 // App Service on Windows: PORT
 // App Service on Linux: WEB_PORT
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const hostname = '0.0.0.0';
 
 server.create()
--- src/server.js
+++ src/server.js
@@ -1,4 +1,38 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+
+const swaggerDocument = {
+    openapi: '3.0.0',
+    info: {
+        title: 'Express API',
+        version: '1.0.0',
+        description: 'API documentation with Swagger UI',
+    },
+    paths: {},
+    components: {
+        securitySchemes: {
+            oauth2: {
+                type: 'oauth2',
+                flows: {
+                    authorizationCode: {
+                        authorizationUrl: 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize',
+                        tokenUrl: 'https://login.microsoftonline.com/common/oauth2/v2.0/token',
+                        scopes: {
+                            'openid': 'OpenID Connect scope',
+                            'profile': 'User profile',
+                            'email': 'User email',
+                        },
+                    },
+                },
+            },
+        },
+    },
+    security: [
+        {
+            oauth2: ['openid', 'profile', 'email']
+        }
+    ],
+};
 const favicon = require('serve-favicon');
 const path = require('path');
 const utils = require('./utils');
@@ -9,13 +43,22 @@ const create = async () => {
     // server
     const app = express();
     app.use(favicon(path.join(__dirname, '../public', 'favicon.ico')));
-    
+    // Swagger UI route with OAuth config and custom redirect URL
+    app.use('/swagger-ui', swaggerUi.serve, swaggerUi.setup(swaggerDocument, false, {
+        oauth: {
+            clientId: 'a81a583b-9276-4c34-bbc5-23717231e67f',
+            scopes: ['openid', 'profile', 'email'],
+            usePkceWithAuthorizationCodeGrant: true,
+        },
+        oauth2RedirectUrl: 'http://localhost:3001/swagger-ui/oauth2-redirect.html',
+    }));
+
     // Log request
     app.use(utils.appLogger);
 
     // root route - serve static file
     app.get('/api/hello', (req, res) => {
-        res.json({hello: 'goodbye'});
+        res.json({ hello: 'Hello' });
         res.end();
     });
 
